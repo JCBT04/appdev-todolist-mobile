7081f970ed4c851463445e23d369fbe9c0993908
jcbt <tevesjhunel01@gmail.com>
2025-04-22 22:10:38 +0800

    first commit

---
+++ frontend/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TodoList from './screens/TodoList.js'
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='todolist'
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name='todolist' component={TodoList} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
